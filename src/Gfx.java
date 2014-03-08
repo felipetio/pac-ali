d5889e0cfc35606f7cdb33dc347f2e8bbc92a508
Felipe Vieira <felipe.tio@gmail.com>
2014-03-08 13:59:49 -0300

    added an absolute path to load the images, fixme!

--- src/Gfx.java
+++ src/Gfx.java
@@ -96,46 +96,49 @@ public class Gfx {
 	public static void uploadImage() {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 
+		// TODO: get project path automatically
+		String project_path = "/Users/felipe/Projects/pac/src/";
+
 		//Fazendo upload de todas imagens...
 
-		pacmamLf = toolkit.getImage("Gfx/pac_Lf.GIF");
-		pacmamRt = toolkit.getImage("Gfx/pac_Rt.GIF");
-		pacmamDw_Lf = toolkit.getImage("Gfx/pac_Dw_Lf.GIF");
-		pacmamDw_Rt = toolkit.getImage("Gfx/pac_Dw_Rt.GIF");
-		pacmamUp_Lf = toolkit.getImage("Gfx/pac_Up_Lf.GIF");
-		pacmamUp_Rt = toolkit.getImage("Gfx/pac_Up_Rt.GIF");
+		pacmamLf = toolkit.getImage(project_path + "Gfx/pac_Lf.GIF");
+		pacmamRt = toolkit.getImage(project_path + "Gfx/pac_Rt.GIF");
+		pacmamDw_Lf = toolkit.getImage(project_path + "Gfx/pac_Dw_Lf.GIF");
+		pacmamDw_Rt = toolkit.getImage(project_path + "Gfx/pac_Dw_Rt.GIF");
+		pacmamUp_Lf = toolkit.getImage(project_path + "Gfx/pac_Up_Lf.GIF");
+		pacmamUp_Rt = toolkit.getImage(project_path + "Gfx/pac_Up_Rt.GIF");
 
-		fantasma0 = toolkit.getImage("Gfx/azul.GIF");
-		fantasma0_super = toolkit.getImage("Gfx/azul_super.GIF");
-		fantasma1 = toolkit.getImage("Gfx/verde.GIF");
-		fantasma1_super = toolkit.getImage("Gfx/verde_super.GIF");
-		fantasma2 = toolkit.getImage("Gfx/laranja.GIF");
-		fantasma2_super = toolkit.getImage("Gfx/laranja_super.GIF");
-		fantasma3 = toolkit.getImage("Gfx/rosa.GIF");
-		fantasma3_super = toolkit.getImage("Gfx/rosa_super.GIF");
+		fantasma0 = toolkit.getImage(project_path + "Gfx/azul.GIF");
+		fantasma0_super = toolkit.getImage(project_path + "Gfx/azul_super.GIF");
+		fantasma1 = toolkit.getImage(project_path + "Gfx/verde.GIF");
+		fantasma1_super = toolkit.getImage(project_path + "Gfx/verde_super.GIF");
+		fantasma2 = toolkit.getImage(project_path + "Gfx/laranja.GIF");
+		fantasma2_super = toolkit.getImage(project_path + "Gfx/laranja_super.GIF");
+		fantasma3 = toolkit.getImage(project_path + "Gfx/rosa.GIF");
+		fantasma3_super = toolkit.getImage(project_path + "Gfx/rosa_super.GIF");
 
-		cherry = toolkit.getImage("Gfx/cherry.GIF");
-		vitamina = toolkit.getImage("Gfx/vit.GIF");
-		vitamina_super = toolkit.getImage("Gfx/vit_super.GIF");
+		cherry = toolkit.getImage(project_path + "Gfx/cherry.GIF");
+		vitamina = toolkit.getImage(project_path + "Gfx/vit.GIF");
+		vitamina_super = toolkit.getImage(project_path + "Gfx/vit_super.GIF");
 
-		block = toolkit.getImage("Gfx/block.GIF");
-		up_dw_lf = toolkit.getImage("Gfx/up_dw_lf.GIF");
-		up_dw_rt = toolkit.getImage("Gfx/up_dw_rt.GIF");
-		up_lf_rt = toolkit.getImage("Gfx/up_lf_rt.GIF");
-		dw_lf_rt = toolkit.getImage("Gfx/dw_lf_rt.GIF");
-		up_dw = toolkit.getImage("Gfx/up_dw.GIF");
-		up_lf = toolkit.getImage("Gfx/up_lf.GIF");
-		up_rt = toolkit.getImage("Gfx/up_rt.GIF");
-		dw_lf = toolkit.getImage("Gfx/dw_lf.GIF");
-		dw_rt = toolkit.getImage("Gfx/dw_rt.GIF");
-		lf_rt = toolkit.getImage("Gfx/lf_rt.GIF");
-		up = toolkit.getImage("Gfx/up.GIF");
-		dw = toolkit.getImage("Gfx/dw.GIF");
-		lf = toolkit.getImage("Gfx/lf.GIF");
-		rt = toolkit.getImage("Gfx/rt.GIF");
-		vazio = toolkit.getImage("Gfx/vazio.GIF");
-		prisao_lf = toolkit.getImage("Gfx/prisao_lf.GIF");
-		prisao_rt = toolkit.getImage("Gfx/prisao_rt.GIF");
+		block = toolkit.getImage(project_path + "Gfx/block.GIF");
+		up_dw_lf = toolkit.getImage(project_path + "Gfx/up_dw_lf.GIF");
+		up_dw_rt = toolkit.getImage(project_path + "Gfx/up_dw_rt.GIF");
+		up_lf_rt = toolkit.getImage(project_path + "Gfx/up_lf_rt.GIF");
+		dw_lf_rt = toolkit.getImage(project_path + "Gfx/dw_lf_rt.GIF");
+		up_dw = toolkit.getImage(project_path + "Gfx/up_dw.GIF");
+		up_lf = toolkit.getImage(project_path + "Gfx/up_lf.GIF");
+		up_rt = toolkit.getImage(project_path + "Gfx/up_rt.GIF");
+		dw_lf = toolkit.getImage(project_path + "Gfx/dw_lf.GIF");
+		dw_rt = toolkit.getImage(project_path + "Gfx/dw_rt.GIF");
+		lf_rt = toolkit.getImage(project_path + "Gfx/lf_rt.GIF");
+		up = toolkit.getImage(project_path + "Gfx/up.GIF");
+		dw = toolkit.getImage(project_path + "Gfx/dw.GIF");
+		lf = toolkit.getImage(project_path + "Gfx/lf.GIF");
+		rt = toolkit.getImage(project_path + "Gfx/rt.GIF");
+		vazio = toolkit.getImage(project_path + "Gfx/vazio.GIF");
+		prisao_lf = toolkit.getImage(project_path + "Gfx/prisao_lf.GIF");
+		prisao_rt = toolkit.getImage(project_path + "Gfx/prisao_rt.GIF");
 
 	}
 
